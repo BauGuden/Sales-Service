9d8dc253a6abe07ae1983bd8e962859874d1e85d
BauGuden <god.caos300@gmail.com>
2026-05-25 16:15:17 -0400

    Validacion implementada

--- src/sales/sales.service.ts
+++ src/sales/sales.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { NatsService } from 'src/common';
 
 @Injectable()
@@ -6,6 +6,25 @@ export class SalesService {
   constructor(private readonly nats: NatsService) {}
 
   async searchPerson(value: string, type: string) {
-    return this.nats.firstValue('person.searchSales', { value, type });
+    const { serviceStatus, error, message, data } = await this.nats.firstValue(
+      'person.searchSales',
+      { value, type },
+    );
+
+    if (!serviceStatus) {
+      return {
+        error: true,
+        statusCode: HttpStatus.SERVICE_UNAVAILABLE,
+        message: 'Servicio de Beneficiarios no disponible',
+        data: null,
+      };
+    }
+
+    return {
+      error,
+      statusCode: error ? HttpStatus.BAD_REQUEST : HttpStatus.OK,
+      message,
+      data: data ?? null,
+    };
   }
 }
