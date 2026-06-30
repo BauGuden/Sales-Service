e59e46960eaa36b311a3049dbfabdacf44277056
ferbautistamollo <ferbautistamollo@gmail.com>
2026-06-30 11:49:11 -0400

    ajustes de nombre d e importación

--- src/sales/entities/index.ts
+++ src/sales/entities/index.ts
@@ -1,8 +1,9 @@
 export { Group } from './group.entity';
-export { PaymentType } from './payment-type.entity';
 export { Parameter } from './parameter.entity';
+export { PaymentType } from './payment-type.entity';
 export { Product } from './product.entity';
 export { QrPayment, QrPaymentStatus } from './qr-payment.entity';
-export { SaleProduct } from './sale-products';
+export { SaleProduct } from './sale-product.entity';
 export { Sale, SaleState } from './sale.entity';
 export { PaymentTypeState, Voucher } from './voucher.entity';
+
